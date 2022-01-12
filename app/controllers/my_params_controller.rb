a791f4b86ab9330164de72c422902bede460e793
ro <kronew6@gmail.com>
2022-01-12 13:53:09 -0600

    adds /name route using parameters

--- app/controllers/my_params_controller.rb
+++ app/controllers/my_params_controller.rb
@@ -1,2 +1,10 @@
 class MyParamsController < ApplicationController
+  def message
+    input = params["name"]
+    if input[0].downcase == "a"
+      render json: { message: "Hey, your name starts with the first letter of the alphabet!" }
+    else
+      render json: { message: "Cool name, #{input}" }
+    end
+  end
 end
